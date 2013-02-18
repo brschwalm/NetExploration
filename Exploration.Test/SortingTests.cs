da2c1b018c60dc1c64d00532c261b1d1961d3973
Brian Schwalm <bschwalm@anythinksolutions.com>
2013-02-18 09:15:20 -0700

    Adding the exploration solution, initialized with some sorting methods

---
+++ Exploration.Test/SortingTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exploration.Test
+{
+	[TestClass]
+	public class SortingTests
+	{
+		[TestMethod]
+		public void Test_BubbleSort()
+		{
+			int[] items = new int[] { 80, 60, 40, 20, 10, 30, 50, 70 };
+			int[] expected = new int[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+			Sorting sorter = new Sorting();
+			int[] actual = sorter.BubbleSort(items);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_BubbleSort_2()
+		{
+			int[] items = new int[] { 80, 60, 120, 40, 20, 90, 100, 10, 30, 110, 50, 70 };
+			int[] expected = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120 };
+
+			Sorting sorter = new Sorting();
+			int[] actual = sorter.BubbleSort(items);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_InsertionSort()
+		{
+			int[] items = new int[] { 80, 60, 40, 20, 10, 30, 50, 70 };
+			int[] expected = new int[] { 10, 20, 30, 40, 50, 60, 70, 80 };
+
+			Sorting sorter = new Sorting();
+			int[] actual = sorter.InsertionSort(items);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_QuickSortIterative()
+		{
+			int[] items = new int[] { 80, 60, 120, 40, 20, 90, 100, 10, 30, 110, 50, 70 };
+			int[] expected = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120 };
+			int length = items.Length - 1;
+
+			Sorting sorter = new Sorting();
+			int[] actual = sorter.QuickSortIterative(items, 0, length);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Test_QuickSortRecursive()
+		{
+			int[] items = new int[] { 80, 60, 120, 40, 20, 90, 100, 10, 30, 110, 50, 70 };
+			int[] expected = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120 };
+			int length = items.Length - 1;
+
+			Sorting sorter = new Sorting();
+			int[] actual = sorter.QuickSortRecursive(items, 0, length);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+	}
+}
---
+++ Exploration/Sorting.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exploration
+{
+	/// <summary>
+	/// Helper to test out and explore different methods of sorting
+	/// </summary>
+    public class Sorting
+    {
+		//*********************************************************************************************
+		//  Helpers Sort
+		//*********************************************************************************************
+
+		public void Shift(int[] array, int index, int direction = -1)
+		{
+			int temp = array[index + direction];
+			array[index + direction] = array[index];
+			array[index] = temp;
+		}
+
+		public void Swap(int[] array, int oldPosition, int newPosition)
+		{
+			int temp = array[oldPosition];
+			array[oldPosition] = array[newPosition];
+			array[newPosition] = temp;
+		}
+
+		//*********************************************************************************************
+		//  Bubble Sort
+		//*********************************************************************************************
+
+		public int[] BubbleSort(int [] items)
+		{
+			if (items == null || items.Length == 0)
+				throw new InvalidOperationException();
+
+			int max = items.Length;
+
+			for (int i = 1; i < max; i++)
+			{
+				for (int j = 0; j < max - i; j++)
+				{
+					if (items[j] > items[j + 1])
+					{
+						Shift(items, j, 1);
+					}
+				}
+			}
+
+			return items;
+		}
+
+		//*********************************************************************************************
+		//  Insertion Sort
+		//*********************************************************************************************
+
+		public int[] InsertionSort(int[] items)
+		{
+			if (items == null || items.Length == 0)
+				throw new InvalidOperationException();
+
+			for (int i = 1; i < items.Length; i++)
+			{
+				int j = i;
+				while (j > 0)
+				{
+					if (items[j - 1] > items[j])
+					{
+						//int temp = items[j - 1];
+						//items[j - 1] = items[j];
+						//items[j] = temp;
+						Shift(items, j);
+						j--;
+					}
+					else
+						break;
+				}
+			}
+
+			return items;
+		}
+
+		//*********************************************************************************************
+		//  Quick Sort
+		//*********************************************************************************************
+		
+		public int Partition(int[] values, int left, int right)
+		{
+			int pivot = values[left];
+			while (true)
+			{
+				while (values[left] < pivot)
+					left++;
+
+				while (values[right] > pivot)
+					right--;
+
+				if (left < right)
+				{
+					Swap(values, left, right);
+				}
+				else
+					return right;
+			}
+		}
+
+		struct QuickPosition
+		{
+			public int Left;
+			public int Right;
+		}
+
+		public int[] QuickSortIterative(int[] items, int left, int right)
+		{
+			if (left >= right)
+				throw new ArgumentException("Left must be less than right.");		//Invalid index range
+
+			List<QuickPosition> list = new List<QuickPosition>();
+			QuickPosition pos = new QuickPosition() { Left = left, Right = right };
+			list.Insert(list.Count, pos);
+
+			while (true)
+			{
+				if (list.Count == 0)
+					break;
+
+				left = list[0].Left;
+				right = list[0].Right;
+				list.RemoveAt(0);
+
+				int pivot = Partition(items, left, right);
+
+				if (pivot > 1)
+				{
+					pos.Left = left;
+					pos.Right = pivot - 1;
+					list.Insert(list.Count, pos);
+				}
+
+				if (pivot + 1 < right)
+				{
+					pos.Left = pivot + 1;
+					pos.Right = right;
+					list.Insert(list.Count, pos);
+				}
+			}
+
+			return items;
+		}
+
+		public int[] QuickSortRecursive(int[] items, int left, int right)
+		{
+			if (left < right)
+			{
+				int pivot = Partition(items, left, right);
+
+				if (pivot > 1)
+					QuickSortRecursive(items, left, pivot - 1);
+
+				if (pivot + 1 < right)
+					QuickSortRecursive(items, pivot + 1, right);
+			}
+
+			return items;
+		}
+    }
+}
